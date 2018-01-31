d7b18d19ec71a61ac98eb9e26711bf3325b01554
Rico Suter <mail@rsuter.com>
2018-01-31 15:56:48 +0100

    v3.12

--- src/NConsole/Properties/AssemblyInfo.cs
+++ src/NConsole/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NConsole")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("3.11.*")]
+[assembly: AssemblyVersion("3.12.*")]
 
 [assembly: InternalsVisibleTo("NConsole.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100796e9bb40e30e212d437841e800959f07c46116dcb2a56cde6b2862d1e6a5c0ca21502eb6388cd02e9647231d0cdcd7e18d13dac7b85582982056d53c5fc1e34079df4ca39eea36a85c1654cbdabb8b1104d099c03dcfbcc99c080a3d7cf536b6705641609c2899f0569a8be1b03aa157dc093d4d3d72604a84bda9c2aa0d4ca")]
